9bafa1df5c650910d0f40acfaf9fe15a373e2a02
Gabriele <gabriele9.rossi@mail.polimi.it>
2023-03-14 17:32:14 +0100

    inserimento metodi model

--- src/main/java/model/Game.java
+++ src/main/java/model/Game.java
@@ -1,8 +1,12 @@
 package model;
 
+import enumerations.GameState;
+import enumerations.TypeSpace;
+import jdk.jshell.spi.ExecutionControl;
 import model.board.Board;
 import model.objects.ObjectsDeck;
 import model.player.Player;
+
 
 import java.util.ArrayList;
 
@@ -11,4 +15,38 @@ public class Game {
     private ArrayList<Player> players;
     private ObjectsDeck objectsDeck;
     private Turn turn;
+    private GameState gameState;
+
+    public void startGame(){
+
+        gameState = GameState.IN_GAME;
+       try { new ExecutionControl.NotImplementedException("not implemented");
+    }catch(Exception ex){new ExecutionControl.NotImplementedException("not implemented");
+    }}
+
+    public boolean isNicknameTaken(String playerNickname){
+
+        for (int i=0; i < players.size(); i++){
+            if(playerNickname.equals(players.get(i).getNickname())){
+                return true;
+            }
+        }return false;
+    }
+
+    public void restoreBoard(Board board){
+        for(int x= 0; x <9; x++){
+            for(int y = 0; y < 9; y++){
+                if(!board.getSpace(x,y).getTypeSpace().equals(TypeSpace.UNUSABLE)){
+                    switch (board.getSpace(x,y).getTypeSpace()){
+                        case FOR_FOUR_PLAYERS -> {
+                            if(players.size()==4){
+
+                                board.putObjectIn();
+                            }
+                        }
+                        }
+                    }
+            }
+        }
+    }
 }
--- src/main/java/model/board/Board.java
+++ src/main/java/model/board/Board.java
@@ -1,8 +1,23 @@
 package model.board;
 
+import model.objects.ObjectCard;
+
 import java.util.ArrayList;
 
 public class Board {
-    private ArrayList<BoardSpace> board;
+    private BoardSpace[][] boardSpaces;
 
+    public BoardSpace getSpace(int x, int y){
+        return boardSpaces[x][y];
+    }
+
+    public ObjectCard pickupObjectFrom(BoardSpace boardSpace){
+        ObjectCard objectPicked = boardSpace.getObject();
+        boardSpace.removeObject();
+        return objectPicked;
+    }
+
+    public void putObjectIn(BoardSpace boardSpace, ObjectCard objectCard){
+        boardSpace.insertObject(objectCard);
+    }
 }
