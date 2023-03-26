8314cda457cb492187b4ec74d715d2e39619ba48
Davide <davide4.spinelli@mail.polimi.it>
2023-03-26 17:56:57 +0200

    Modified Player constructor to include the personal objective assignation

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -12,8 +12,8 @@ public class GameTest {
     @Before
     public void setUp(){
         this.instance = new Game();
-        Player p1 = new Player("Alice");
-        Player p2 = new Player("Bob");
+        Player p1 = new Player("Alice","1,1,PINK1,2,0,GREEN1,2,2,YELLOW1,3,4,WHITE1,4,3,LIGHT_BLUE1,5,4,BLUE1");
+        Player p2 = new Player("Bob","1,0,BLUE1,1,3,YELLOW1,2,2,PINK1,3,1,GREEN1,3,4,LIGHT_BLUE1,5,0,WHITE1");
 
         instance.addToGame(p1);
         instance.addToGame(p2);
@@ -21,14 +21,14 @@ public class GameTest {
 
     @Test
     public void addToGame_OK(){
-        instance.addToGame(new Player("Carlo"));
+        instance.addToGame(new Player("Carlo","0,0,PINK1,0,2,BLUE1,1,4,GREEN1,2,3,WHITE1,3,1,YELLOW1,5,2,LIGHT_BLUE1"));
 
         assertEquals(3, instance.numOfPlayers());
     }
 
     @Test
     public void addNullPlayer(){
-        instance.addToGame(new Player(null));
+        instance.addToGame(new Player(null,null));
 
         assertEquals(2, instance.numOfPlayers());
     }
