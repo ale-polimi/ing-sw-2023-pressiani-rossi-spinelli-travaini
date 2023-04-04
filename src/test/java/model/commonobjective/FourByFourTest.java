a5239f704902c1d71cc3489b633873868d685d77
Gabriele <gabriele9.rossi@mail.polimi.it>
2023-04-04 17:37:28 +0200

    Added isEquals for commonObjecives and fixed FourByFour

--- src/test/java/model/commonobjective/FourByFourTest.java
+++ src/test/java/model/commonobjective/FourByFourTest.java
@@ -26,34 +26,34 @@ public class FourByFourTest {
             }
         }
 
-        instance.addObject(new ObjectCard("GREEN1"),instance.getLibrarySpace(0,0));
-        instance.addObject(new ObjectCard("GREEN1"),instance.getLibrarySpace(0,1));
+        instance.addObject(new ObjectCard("GREEN2"),instance.getLibrarySpace(0,0));
+        instance.addObject(new ObjectCard("GREEN3"),instance.getLibrarySpace(0,1));
         instance.addObject(new ObjectCard("GREEN1"),instance.getLibrarySpace(0,2));
         instance.addObject(new ObjectCard("GREEN1"),instance.getLibrarySpace(0,3));
         instance.addObject(new ObjectCard("WHITE1"),instance.getLibrarySpace(0,4));
         instance.addObject(new ObjectCard("GREEN1"),instance.getLibrarySpace(1,0));
         instance.addObject(new ObjectCard("BLUE1"),instance.getLibrarySpace(1,1));
-        instance.addObject(new ObjectCard("BLUE1"),instance.getLibrarySpace(1,2));
+        instance.addObject(new ObjectCard("BLUE3"),instance.getLibrarySpace(1,2));
         instance.addObject(new ObjectCard("BLUE1"),instance.getLibrarySpace(1,3));
         instance.addObject(new ObjectCard("BLUE1"),instance.getLibrarySpace(1,4));
         instance.addObject(new ObjectCard("GREEN1"),instance.getLibrarySpace(2,0));
-        instance.addObject(new ObjectCard("GREEN1"),instance.getLibrarySpace(2,1));
-        instance.addObject(new ObjectCard("GREEN1"),instance.getLibrarySpace(2,2));
+        instance.addObject(new ObjectCard("YELLOW1"),instance.getLibrarySpace(2,1));
+        instance.addObject(new ObjectCard("WHITE1"),instance.getLibrarySpace(2,2));
         instance.addObject(new ObjectCard("GREEN1"),instance.getLibrarySpace(2,3));
         instance.addObject(new ObjectCard("YELLOW1"),instance.getLibrarySpace(2,4));
         instance.addObject(new ObjectCard("LIGHT_BLUE1"),instance.getLibrarySpace(3,0));
         instance.addObject(new ObjectCard("GREEN1"),instance.getLibrarySpace(3,1));
         instance.addObject(new ObjectCard("GREEN1"),instance.getLibrarySpace(3,2));
-        instance.addObject(new ObjectCard("GREEN1"),instance.getLibrarySpace(3,3));
+        instance.addObject(new ObjectCard("WHITE1"),instance.getLibrarySpace(3,3));
         instance.addObject(new ObjectCard("GREEN1"),instance.getLibrarySpace(3,4));
         instance.addObject(new ObjectCard("GREEN1"),instance.getLibrarySpace(4,0));
         instance.addObject(new ObjectCard("GREEN1"),instance.getLibrarySpace(4,1));
-        instance.addObject(new ObjectCard("GREEN1"),instance.getLibrarySpace(4,2));
+        instance.addObject(new ObjectCard("GREEN3"),instance.getLibrarySpace(4,2));
         instance.addObject(new ObjectCard("GREEN1"),instance.getLibrarySpace(4,3));
         instance.addObject(new ObjectCard("GREEN1"),instance.getLibrarySpace(4,4));
         instance.addObject(new ObjectCard("GREEN1"),instance.getLibrarySpace(5,0));
         instance.addObject(new ObjectCard("GREEN1"),instance.getLibrarySpace(5,1));
-        instance.addObject(new ObjectCard("GREEN1"),instance.getLibrarySpace(5,2));
+        instance.addObject(new ObjectCard("GREEN2"),instance.getLibrarySpace(5,2));
         instance.addObject(new ObjectCard("GREEN1"),instance.getLibrarySpace(5,3));
         instance.addObject(new ObjectCard("GREEN1"),instance.getLibrarySpace(5,4));
 
@@ -71,8 +71,9 @@ public class FourByFourTest {
         int x = 0;
         int y = 0;
         fourByFour.applyObjectiveRules(instance,x,y);
-        if(fourByFour.getCount()==4)
-            assertTrue(fourByFour.applyObjectiveRules(instance,x,y));
+        if(fourByFour.getCount()==4){
+            System.out.print("fourbyfour");
+            assertTrue(fourByFour.applyObjectiveRules(instance,x,y));}
     }
 
     @Test
@@ -80,8 +81,9 @@ public class FourByFourTest {
         int x = 0;
         int y = 0;
         fourByFour.applyObjectiveRules(instance,x,y);
-        if(fourByFour.getCount()<4)
-            assertFalse(fourByFour.applyObjectiveRules(instance,x,y));
+        if(fourByFour.getCount()<4){
+            System.out.print("notfourbyfour");
+            assertFalse(fourByFour.applyObjectiveRules(instance,x,y));}
     }
 
 }
