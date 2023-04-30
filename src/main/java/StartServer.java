0cf63c9e6b88c85d79e20444320d9b6ac7d1e58c
Davide Spinelli <davide4.spinelli@mail.polimi.it>
2023-04-30 19:19:35 +0200

    Deleted StarServer and StartServerImpl as they are obsolete

--- src/main/java/StartServer.java
+++
@@ -1,8 +0,0 @@
-import network.structure.Server;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface StartServer extends Remote {
-    Server connect() throws RemoteException;
-}
