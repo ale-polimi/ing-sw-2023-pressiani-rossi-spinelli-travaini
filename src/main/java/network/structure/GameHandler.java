29cb0f550b09b2a362f86a68baf2541e12214923
Davide Spinelli <davide4.spinelli@mail.polimi.it>
2023-04-30 20:54:29 +0200

    Created GameHandler class to manage the calls to the clients

---
+++ src/main/java/network/structure/GameHandler.java
@@ -0,0 +1,12 @@
+package network.structure;
+
+public class GameHandler {
+    private final Tuple game;
+    public GameHandler(Tuple game){
+        this.game = game;
+    }
+
+    public void start(){
+        /*TODO gestione dello svolgimento della partita*/
+    }
+}
