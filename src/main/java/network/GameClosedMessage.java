43feb44dffa5926ce5618335e73373ae2e8f232e
Davide Spinelli <davide4.spinelli@mail.polimi.it>
2023-05-13 17:31:31 +0200

    New message to handle connection with the server

---
+++ src/main/java/network/GameClosedMessage.java
@@ -0,0 +1,13 @@
+package network;
+
+public class GameClosedMessage extends Message{
+
+    /**
+     * Constructor for  class GameClosedMessage.
+     * @param sender      is the sender of the message.
+     * @param messageType is the type of the message.
+     */
+    public GameClosedMessage(String sender, MessageType messageType) {
+        super(sender, messageType);
+    }
+}
---
+++ src/main/java/network/PingMessage.java
@@ -0,0 +1,12 @@
+package network;
+
+public class PingMessage  extends Message{
+    /**
+     *Custom constructor for class PingMessage
+     * @param sender      is the sender of the message.
+     * @param messageType is the type of the message.
+     */
+    public PingMessage(String sender, MessageType messageType) {
+        super(sender, messageType);
+    }
+}
