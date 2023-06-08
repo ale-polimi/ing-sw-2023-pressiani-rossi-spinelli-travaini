b4c0f0356d35762da12385a6684aadc33eef1b8b
ale-polimi <10742196@polimi.it>
2023-06-08 13:33:36 +0200

    Removed unused class; updated client main to accept and parse the command line parameters (now commented out for faster debugging)

--- src/main/java/network/structure/ClientMain.java
+++ src/main/java/network/structure/ClientMain.java
@@ -7,34 +7,84 @@ import controller.ClientController;
 import view.cli.*;
 
 
-
+/**
+ * Main class of the client application. The user can decide what type of client to launch with the following command line parameters:
+ * <ul>
+ *     <li>First parameter:</li>
+ *     <ul>
+ *         <li>"{@code -cli}" or "{@code -c}" will start the CLI game;</li>
+ *         <li>"{@code -gui}" or "{@code -g}" will start the GUI game;</li>
+ *     </ul>
+ *     <li>Second parameter:</li>
+ *     <ul>
+ *         <li>"{@code -socket}" or "{@code -s}" will start the game based on a socket connection;</li>
+ *         <li>"{@code -rmi}" or "{@code -r}" will start the game based on a RMI connection;</li>
+ *     </ul>
+ * </ul>
+ */
 public class ClientMain {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        boolean isCli=true;
+        /* TODO - REMOVE COMMENT HERE FOR FINAL DEPLOYMENT */
+/*
+        boolean isCli = true;
+        boolean isSocket = true;
+        boolean failedArgs = false;
 
+        if (args.length != 2) {
+            System.out.println("" + Colours.RED + Colours.BOLD + "Invalid number of parameters. Required: 2; Provided: " + args.length + Colours.RESET);
+            failedArgs = true;
+        } else {
 
-        //check if CLI or GUI and if is CLI set isCli to true
-       // for (String arg: args){
-         //   if (arg.equals("CLI"))
-          //      isCli=true;
-        //    break;
-        //}
+            *//* Check for the first argument *//*
+            if (args[0].equals("-cli") || args[0].equals("-c")) {
+                isCli = true;
+            } else if (args[0].equals("-gui") || args[0].equals("-g")) {
+                isCli = false;
+            } else {
+                System.out.println("" + Colours.RED + Colours.BOLD + "Invalid parameter: " + args[0] + Colours.RESET);
+                failedArgs = true;
+            }
 
-        //if is Cli, create a cli
+            *//* Check for the second argument *//*
+            if (args[1].equals("-socket") || args[1].equals("-s")) {
+                isSocket = true;
+            } else if (args[1].equals("-rmi") || args[1].equals("-r")) {
+                isSocket = false;
+            } else {
+                System.out.println("" + Colours.RED + Colours.BOLD + "Invalid parameter: " + args[1] + Colours.RESET);
+                failedArgs = true;
+            }
+        }
 
-        if (isCli){
+        if (!failedArgs) {
+            if (isCli) {
+                Cli view = new Cli();
+                ClientController clientController = new ClientController(view, isSocket);
+                view.addObserver(clientController);
+                view.initCli();
+            } else {
+                *//* TODO - GUI application launch *//*
+                //Application.launch(Gui.class);
+            }
+        } else {
+            System.exit(2);
+        }
+*/
+
+        /* TODO - REMOVE THIS! ONLY FOR DEBUG */
+        boolean isCli = true;
+        boolean isSocket = true;
+
+        if (isCli) {
             Cli view = new Cli();
-            ClientController clientController = new ClientController(view,true);
+            ClientController clientController = new ClientController(view, isSocket);
             view.addObserver(clientController);
             view.initCli();
+        } else {
+            /* TODO - GUI application launch */
+            //Application.launch(Gui.class);
         }
-
-        //launch a GUI
-      if (!isCli){
-          //Application.launch(Gui.class);
-        }
-
     }
 
 }
